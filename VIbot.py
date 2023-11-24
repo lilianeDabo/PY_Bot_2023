927b9a4c45995bf61bc15593ed2b38084b48e6f9
Liliane Dabo <lilianedabo@outlook.com>
2023-11-24 17:43:27 +0100

    weather API

--- VIbot.py
+++ VIbot.py
@@ -1,5 +1,9 @@
 import discord
-from Key import key
+import requests
+import json
+from meteofrance_api import *
+
+from Key import *
 from discord.ext import commands
 
 #autres fichiers
@@ -30,18 +34,27 @@ async def on_ready():
     print("𝔹𝕠𝕥𝕤 𝕒𝕣𝕖 𝕣𝕖𝕒𝕕𝕪")
     print("--------------")
 
+
+
 @client.event
 async def on_typing(channel, user, when):
      await channel.send(user.name+" is typing")
 
 @client.event
 async def on_member_join(member):
-    general_channel = client.get_channel(977137496720826368) #get_channel(channel x's id)
-    await general_channel.send("Bienvenue sur le serveur ! "+ member.name) 
+    
+    general_channel = client.get_channel(1167470292088660020) #get_channel(channel x's id)
+    await general_channel.send(member.name + " , welcome !")
+    Meteo = MeteoFranceClient()
+    Forecast = Meteo.get_forecast(longitude=48.864716, latitude=2.349014)
+    # print(Forecast.current_forecast)
+    desc_weather = Forecast.current_forecast.get("weather", {}).get("desc")
+    # print(desc_weather)
+    await general_channel.send("Here is the current forecast : " + desc_weather)
 
 @client.event
 async def on_member_remove(member):
-    general_channel = client.get_channel(977137496720826368) #get_channel(channel x's id)
+    general_channel = client.get_channel(1167470292088660020) #get_channel(channel x's id)
     await general_channel.send("Farewell  " + member.name + " !") 
 
 @client.event
